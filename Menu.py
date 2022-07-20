5c2e6b9fe66a81093637b5fa1d94df56a2bc24ac
TheRedBasic <87467849+TheRedBasic@users.noreply.github.com>
2022-07-20 08:55:09 -0600

    Create Menu.py

---
+++ Menu.py
@@ -0,0 +1,3 @@
+#This is a program that will be used for a majority of the program(s), it will support many lists, descriptions, titles, inputs, etc.
+#I will document how to use different modules with this commentary
+class Multiple_Option(self)
